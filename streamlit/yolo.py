b1e0c9161d4c22376239a638470f1e7f0aa1a93f
gulnazds <kidrasova.ga@gmail.com>
2022-10-21 12:52:51 +0300

    yolo streamlit

---
+++ streamlit/yolo.py
@@ -0,0 +1,22 @@
+from ast import increment_lineno
+import torch
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+st.write("""
+# Cервис детекции козочек, сторожевых собак и куриц
+### по фотографиям фермы 
+""")
+
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/gulnaz/ds_offline/cv_project/res_models/5yolo_best.pt')
+img_file = st.file_uploader('Choose file', type=['png', 'jpg', 'jpeg'], accept_multiple_files=False)
+if img_file:
+    img = Image.open(img_file)
+    results = model(img)
+    fig, ax = plt.subplots()
+    plt.axis('off')
+    ax.imshow(results.render()[0])
+    st.pyplot(fig)
